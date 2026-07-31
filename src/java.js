4fbe8d282fb55f342c35e67422dbeaa90e04959c
rinathouin <rina.thouin@gmail.com>
2026-07-31 12:20:08 -0700

    update html and java [skip ci]
    [skip ci]

--- src/java.js
+++ src/java.js
@@ -6,13 +6,36 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let speedElement = document.querySelector("#speed");
   let speed = response.data.wind.speed;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
-  speedElement.innerHTML = Math.round(speed);
+  speedElement.innerHTML = `${Math.round(speed)}mph`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}, `;
+}
+
 function searchCity(city) {
   let apiKey = "f18o339a0b4b86f5a773b5031a4d08t4";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
