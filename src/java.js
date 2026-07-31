0529bf809a205fb3577d1ec21eeb936ccb373708
rinathouin <rina.thouin@gmail.com>
2026-07-31 09:19:15 -0700

    added java and axios [skip ci]
    added java and api url [skip ci]

--- src/java.js
+++ src/java.js
@@ -1,2 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "f18o339a0b4b86f5a773b5031a4d08t4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-input");
+
+  searchCity(searchInput.value);
+}
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Gilbert");
